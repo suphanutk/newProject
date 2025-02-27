bd527ece1ee24a17027aa05fccdb873bc98cb4aa
game1 <supanut.gg2022@gmail.com>
2025-02-27 16:37:58 +0700

    First commit

---
+++ src/data/data_prep.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({"A": [1,2,3,4,5], "B": [6,7,8,9,10]})
+print(df) 
+
+C:\Users\suphanutk\Desktop\ProjectML\ds-project
